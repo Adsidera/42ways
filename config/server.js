b6a1bebff9a19862566d16d08933fe5c5d92b2da
Adsidera <annamaria.costalonga@gmail.com>
2020-11-27 06:33:30 +0100

    Installed pg, aws-s3 upload provider and updated config files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '49707126319884cceab7a15b6625dc9d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5d5bf52a-45d5-4467-b1c5-570672efc97e'
+};
